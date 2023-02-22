e4767dc097ac2908674b03468f7ccb58bf89522c
Oleksii Bezkorovainyi <bd.kiev@gmail.com>
2023-02-22 23:17:29 +0200

    Did Lesson-21

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstNumber = prompt('Enter first number');
+let secondNumber = prompt('Enter second number');
+
+const resultSumm = Number(firstNumber) + Number(secondNumber);
+const resultDifference = firstNumber - secondNumber;
+const resultMultiply = firstNumber * secondNumber;
+const resultDivison = firstNumber / secondNumber;
+
+alert(`Your result is Summ - ${resultSumm}, Difference - ${resultDifference}, Multiply - ${resultMultiply}, Divide - ${resultDivison}`);
