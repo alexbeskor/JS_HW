78f127e88feb2ab2df118e8a2c726d5c4d1e1bdc
Oleksii Bezkorovainyi <bd.kiev@gmail.com>
2023-02-25 18:56:02 +0200

    Did Lesson-22

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ const resultDifference = firstNumber - secondNumber;
 const resultMultiply = firstNumber * secondNumber;
 const resultDivison = firstNumber / secondNumber;
 
-alert(`Your result is Summ - ${resultSumm}, Difference - ${resultDifference}, Multiply - ${resultMultiply}, Divide - ${resultDivison}`);
+let isAllOk;
+
+if (secondNumber === 0) {
+    alert('Error: Second number is 0');
+} else if ((firstNumber === '' || firstNumber === null) || (secondNumber === '' || secondNumber === null)) {
+    alert('Error: One of fields is empty');    
+} else if (firstNumber < secondNumber) {
+    isAllOk = confirm('Are you sure you want to continue calculation?');
+    if (isAllOk === true) {
+        alert((`Summ of ${firstNumber} and ${secondNumber} is ${resultSumm}`));
+        alert((`Difference of ${firstNumber} and ${secondNumber} is ${resultDifference}`));
+        alert((`Multiplification of ${firstNumber} and ${secondNumber} is ${resultMultiply}`));
+        alert((`Division of ${firstNumber} and ${secondNumber} is ${resultDivison}`));
+    }
+} 
+    else {
+    alert(`Summ of ${firstNumber} and ${secondNumber} is ${resultSumm}`);
+    alert(`Difference of ${firstNumber} and ${secondNumber} is ${resultDifference}`);
+    alert(`Multiplification of ${firstNumber} and ${secondNumber} is ${resultMultiply}`);
+    alert(`Division of ${firstNumber} and ${secondNumber} is ${resultDivison}`);
+}
+
+// alert(`Your result is Summ - ${resultSumm}, Difference - ${resultDifference}, Multiply - ${resultMultiply}, Divide - ${resultDivison}`);
